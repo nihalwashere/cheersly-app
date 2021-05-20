e2b2c52f92c9f3705c966e41e641cfbe7464b207
pikure <nihalk2@hotmail.com>
2021-05-20 19:06:36 +0530

    fix: prod config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,29 +1,29 @@
 // -------------------- PRODUCTION ---------------------
 
-// export const CHEERSLY_API_BASE_URL_V1 = "https://cheersly.herokuapp.com/api/v1"; // prod
+export const CHEERSLY_API_BASE_URL_V1 = "https://cheersly.herokuapp.com/api/v1"; // prod
 
-// export const CHEERSLY_GRAPHQL_BASE_URL =
-//   "https://cheersly.herokuapp.com/graphql"; // prod
+export const CHEERSLY_GRAPHQL_BASE_URL =
+  "https://cheersly.herokuapp.com/graphql"; // prod
 
-// export const SLACK_CLIENT_ID = "965550414273.1557391535763"; // prod
+export const SLACK_CLIENT_ID = "965550414273.1557391535763"; // prod
 
-// export const SLACK_CLIENT_SECRET = "42aab2130c372391452e5b925bf58cc8"; // prod
+export const SLACK_CLIENT_SECRET = "42aab2130c372391452e5b925bf58cc8"; // prod
 
-// export const SLACK_REDIRECT_URI = "https://app.cheersly.club/auth"; // prod
+export const SLACK_REDIRECT_URI = "https://app.cheersly.club/auth"; // prod
 
 // -------------------- DEVELOP ------------------------
 
-export const CHEERSLY_API_BASE_URL_V1 =
-  "https://cheersly-dev.herokuapp.com/api/v1"; // dev
+// export const CHEERSLY_API_BASE_URL_V1 =
+//   "https://cheersly-dev.herokuapp.com/api/v1"; // dev
 
-export const CHEERSLY_GRAPHQL_BASE_URL =
-  "https://cheersly-dev.herokuapp.com/graphql"; // dev
+// export const CHEERSLY_GRAPHQL_BASE_URL =
+//   "https://cheersly-dev.herokuapp.com/graphql"; // dev
 
-export const SLACK_REDIRECT_URI = "https://app-dev.cheersly.club/auth"; // dev
+// export const SLACK_REDIRECT_URI = "https://app-dev.cheersly.club/auth"; // dev
 
-export const SLACK_CLIENT_ID = "1292560080950.1764108002000"; // dev
+// export const SLACK_CLIENT_ID = "1292560080950.1764108002000"; // dev
 
-export const SLACK_CLIENT_SECRET = "ecd30e96dcd31b871ee7c6c3fbbb8618"; // dev
+// export const SLACK_CLIENT_SECRET = "ecd30e96dcd31b871ee7c6c3fbbb8618"; // dev
 
 // -------------------- LOCAL ------------------------
 
