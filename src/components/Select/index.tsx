3b476e0b8f4bec9d4826dd201b076c66e0a2112c
pikure <nihalk2@hotmail.com>
2022-02-15 00:47:54 +0530

    fix

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,7 +1,6 @@
-import React from "react";
 import { styled } from "@mui/material/styles";
 import MuiSelect from "@mui/material/Select";
 
-const Select = styled(MuiSelect)(({ theme }) => ({}));
+const Select = styled(MuiSelect)(() => ({}));
 
 export default Select;
