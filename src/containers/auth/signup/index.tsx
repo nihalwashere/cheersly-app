fbd5ff98d02bd3ac878b57626ed3cc4c0f833565
nihalwashere <nihal.kaul@sjsu.edu>
2023-07-17 18:53:02 -0700

    fix

--- src/containers/auth/signup/index.tsx
+++ src/containers/auth/signup/index.tsx
@@ -16,6 +16,10 @@ export default function SignUp() {
   //   setTosChecked(event.target.checked);
   // };
 
+  const redirectOauth = () => {
+    window.location.href = process.env.REACT_APP_SLACK_SIGNUP_URL || "";
+  };
+
   return (
     <div className="flex h-screen w-full bg-yellow-extra-light">
       <div className="w-1/2 flex flex-col items-center justify-center">
@@ -53,16 +57,14 @@ export default function SignUp() {
           </div>
         </div> */}
 
-        <div className="mt-10 flex justify-center">
-          <a href={process.env.REACT_APP_SLACK_SIGNUP_URL}>
-            <img
-              alt="Add to Slack"
-              height="40"
-              width="139"
-              src="https://platform.slack-edge.com/img/add_to_slack.png"
-              srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
-            />
-          </a>
+        <div className="mt-10 flex justify-center" onClick={redirectOauth}>
+          <img
+            alt="Add to Slack"
+            height="40"
+            width="139"
+            src="https://platform.slack-edge.com/img/add_to_slack.png"
+            srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+          />
         </div>
 
         <div className="mt-10">
